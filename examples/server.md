6188a355c5e5d1e0e80109ce6bf4e42b3772514b
Joost Yervante Damad <joost@damad.be>
2016-12-28 08:33:46 +0100

    make main into a standalone server example

---
+++ examples/server.md
@@ -0,0 +1,4 @@
+In order for the example server to work, add the following line to your
+/etc/hosts file:
+
+127.0.3.1       website1.local website2.local
---
+++ examples/server.rs
@@ -0,0 +1,32 @@
+extern crate iron_static_server;
+#[macro_use]
+extern crate log;
+extern crate env_logger;
+
+use std::env;
+
+fn main() {
+    env_logger::init().unwrap();
+    let mut dirname = String::new();
+    dirname.push_str(env!("CARGO_MANIFEST_DIR"));
+    dirname.push_str("/examples");
+    env::set_current_dir(&dirname).unwrap();
+    let filename = "server.toml";
+    if let Err(ref e) = iron_static_server::run(&filename) {
+        use ::std::io::Write;
+        let stderr = &mut ::std::io::stderr();
+        let errmsg = "Error writing to stderr";
+
+        writeln!(stderr, "error: {}", e).expect(errmsg);
+
+        for e in e.iter().skip(1) {
+            writeln!(stderr, "caused by: {}", e).expect(errmsg);
+        }
+
+        if let Some(backtrace) = e.backtrace() {
+            writeln!(stderr, "backtrace: {:?}", backtrace).expect(errmsg);
+        }
+
+        ::std::process::exit(1);
+    }
+}
